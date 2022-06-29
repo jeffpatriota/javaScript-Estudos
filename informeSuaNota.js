9bec4bdce1e4e667e22db80510259587f44a4ab9
jeffpatriota <jefferson.patriota@outlook.com>
2022-06-29 13:13:27 -0300

    Praticando IfElse

---
+++ informeSuaNota.js
@@ -0,0 +1,9 @@
+const informeSuaNota = function (nota){
+    if(nota >= 7){
+        console.log('Aprovado');
+    }else{
+        console.log('Reprovado');
+    }
+}
+informeSuaNota(8);
+informeSuaNota(6);
