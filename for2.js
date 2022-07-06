afb2b1a182b7614df8b1e5cd15fcacece270b361
jeffpatriota <jefferson.patriota@outlook.com>
2022-07-06 12:28:02 -0300

    Praticando ForIn

---
+++ for2.js
@@ -0,0 +1,15 @@
+const notas = [6.4, 8.9, 4.8, 1.6, 2.3]
+
+for (let i in notas){
+    console.log(i, notas[i])
+}
+
+const pessoa = {
+    nome: 'Jefferson',
+    sobrenome: 'Patriota',
+    idade: 35,
+    peso: 73
+}
+    for(let atributo in pessoa){
+        console.log(`${atributo} = ${pessoa[atributo]}`)
+    }
