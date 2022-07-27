1337d131c692ea5272b9fecf80358f1a31ad1ac4
jeffpatriota <jefferson.patriota@outlook.com>
2022-07-27 18:22:55 -0300

    funcao

---
+++ funcao/exer1.js
@@ -0,0 +1,8 @@
+//Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+//multiplicação e divisão desses valores.
+
+function calcularOperacoes (operador1, opreador2){
+    console.log(operador1 + opreador2, operador1 - opreador2, operador1 * opreador2, operador1 / opreador2 )
+}
+
+calcularOperacoes(2, 2)
